7932954e114973e5e727f9fe46d997b933c04f85
DmitryMurashov <dmtr.murashov@gmail.com>
2026-04-11 05:27:35 -0300

    Revert "experiments w/ build"
    This reverts commit 52a6a5b6de1418f6292021c52f8be5a80e5a8915.

--- HAL/MCU.c
+++ HAL/MCU.c
@@ -13,7 +13,7 @@
 /******************************************************************************/
 /* Header file contains */
 #include "HAL.h"
-// #include <string.h>
+#include "string.h"
 
 tmosTaskID halTaskID;
 uint32_t g_LLE_IRQLibHandlerLocation;
@@ -226,7 +226,7 @@ tmosEvents HAL_ProcessEvent(tmosTaskID task_id, tmosEvents events)
 /*******************************************************************************
  * @fn      HAL_Init
  *
- * @brief   Ó²ï¿½ï¿½ï¿½ï¿½Ê¼ï¿½ï¿½
+ * @brief   Ó²¼þ³õÊ¼»¯
  *
  * @param   None.
  *
